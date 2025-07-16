f9843bbe7ad88cd419f001dbea2d5817144ee5fb
joch 0117 <jochaigneau1@gmail.com>
2025-07-16 13:57:25 +0200

    ajout supr tableau front ok

--- front-end/asset/js/boardManager.js
+++ front-end/asset/js/boardManager.js
@@ -21,7 +21,18 @@ export default class BoardManager {
             const li = document.createElement('li');
             li.className = 'board-list-item';
             li.dataset.boardId = board.id;
-            li.innerHTML = `<span class="board-title" contenteditable="true">${board.title}</span>`;
+            li.innerHTML = 
+            `<span class="board-title" contenteditable="true">${board.title}</span>
+            <button class="delete-board-btn" title="Supprimer ce tableau" style="background: none; border: none; cursor: pointer; padding-left: 0.5em; vertical-align: middle;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24">
+            <path fill="#b92127" d="M7 21a2 2 0 0 1-2-2V7H3V5h5V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h5v2h-2v12a2 2 0 0 1-2 2H7Zm10-14H7v12h10V7Zm-4 2v8h-2V9h2Z"/>
+            </svg>
+            </button>`;
+
+            li.querySelector('.delete-board-btn').onclick = (e) =>{
+                e.stopPropagation();
+                this.deleteBoard(board.id)
+            }
 
             li.onclick = () => this.selectBoard(board.id);
 
@@ -32,6 +43,15 @@ export default class BoardManager {
             };
             sidebarList.appendChild(li);
         });
+    }
+
+    deleteBoard(id) {
+        this.boards = this.boards.filter(b => b.id !== id);
+        if (this.currentBoardId === id) {
+            this.currentBoardId = this.boards.length ? this.boards[0].id : null;
+        }
+        this.renderBoards();
+        this.renderBoardContent();
     }
 
     selectBoard(id){
